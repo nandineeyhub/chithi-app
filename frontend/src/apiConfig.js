c954283e6c16e73365005c551c54ce13612e83b7
nandineeyhub <nandinijha1519@gmail.com>
2024-04-08 14:08:55 +0530

    add changes

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,13 +1,15 @@
 export const baseURL = "http://localhost:8000/api/";
 
 export const apiUrls = {
-    login:"users/login",
-    register:"users/register"
-}
+  login: "users/login",
+  register: "users/register",
+  uploadProfilePicture: "users/upload-picture",
+  removeProfilePicture: "users/remove-picture",
+};
 
-const token = localStorage.getItem("userData")?.token
+const token = localStorage.getItem("userData")?.token;
 
 export const headers = {
-    'Authorization': `Bearer ${token}`,
-    'Content-Type': 'application/json' // example header
-  };
+  Authorization: `Bearer ${token}`,
+  "Content-Type": "application/json", // example header
+};
