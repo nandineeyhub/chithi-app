3c3a28220cfa0cebe2f2a63e0f6814da43dc8c6b
Nandini <nandinijha1519@gmail.com>
2024-06-29 14:09:51 +0530

    add start commans

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -1,8 +1,8 @@
 
 
-export const baseURL = "http://localhost:8000/api/";
-export const imgUrl = "http://localhost:8000/api/users/images/"
-export const base = "http://localhost:8000"
+export const baseURL = "https://ping-chats.vercel.app/api/";
+export const imgUrl = "https://ping-chats.vercel.app/api/users/images/"
+export const base = "https://ping-chats.vercel.app"
 export const apiUrls = {
   
   login: "users/login",
